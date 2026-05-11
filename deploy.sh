610a60c2ef4d7a80fc752c581cc0ef8e8231ee9b
Chirag Gowda <contact@chiraggowda.com>
2026-05-11 09:56:20 +0530

    rating update

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-git checkout chirag
-git pull origin chirag
+git checkout back-end-1
+git pull origin back-end-1
 
 git checkout prod
-git checkout chirag -- backend
+git checkout back-end-1 -- backend
 git add .
 git commit -m "updated"
 git push origin prod
