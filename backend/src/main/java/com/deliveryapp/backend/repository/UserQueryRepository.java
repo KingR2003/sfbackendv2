2355f368ea0149fb6d3bea52af6a32843255d687
Chirag Gowda <contact@chiraggowda.com>
2026-05-13 13:21:05 +0530

    admin mobile number fix

--- backend/src/main/java/com/deliveryapp/backend/repository/UserQueryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.deliveryapp.backend.repository;
-
-import com.deliveryapp.backend.entity.UserQuery;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserQueryRepository extends JpaRepository<UserQuery, Long> {
-}
